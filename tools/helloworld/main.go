5ef5106c83802e8e450b198e55c38c32cfc05462
skiffer-git <44203734@qq.com>
2024-04-11 10:07:34 +0800

    Project initialization

---
+++ tools/helloworld/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	index := flag.Int("i", 0, "Index number")
+	config := flag.String("c", "", "Configuration directory")
+	// Parse the flags
+	flag.Parse()
+	fmt.Println("this is hello world for tools")
+	fmt.Printf("Program name: %s, args: -i %d -c %s\n", os.Args[0], *index, *config)
+}
