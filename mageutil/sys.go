86c065fdfd8901d69cddf2abf7be37bc93792efa
skiffer-git <44203734@qq.com>
2024-05-07 20:39:54 +0800

    kill binaries before build

--- mageutil/sys.go
+++ mageutil/sys.go
@@ -140,12 +140,9 @@ func BatchKillExistBinaries(binaryPaths []string) {
 			continue // Skip processes where the executable path cannot be determined
 		}
 		exePathMap[exePath] = append(exePathMap[exePath], p)
-		fmt.Println("exePath im map ", exePath)
 	}
 
 	for _, binaryPath := range binaryPaths {
-		fmt.Println("binaryPath  ", binaryPath)
-
 		if procs, found := exePathMap[binaryPath]; found {
 			fmt.Println("binaryPath  found ", binaryPath)
 			for _, p := range procs {
